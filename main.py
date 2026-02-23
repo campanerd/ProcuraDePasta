f1eae130b96932e59f9af481fdafdf75ea68fd85
Davi Campaner <campanerdavi@gmail.com>
2026-02-23 19:12:24 -0300

    feat: add partial SFTP folder search

--- main.py
+++ main.py
@@ -8,53 +8,56 @@ from pathlib import Path
 
 BASE_REMOTA = "/sftp-assessoria-prd/Fortes-Assessoria/Cobaas/Enviados/Documentos"
 
+def encontrar_pasta_contrato(sftp, base_remota, termo_busca):
+    pastas = sftp.listdir_attr(base_remota)
+    for attr in pastas:
+        if termo_busca.lower() in attr.filename.lower():
+            return f"{base_remota}/{attr.filename}"
+    return None
 
 def executar_contrato(contrato: str) -> str:
     if not contrato:
         return "Contrato não informado."
 
-    pasta_remota = f"{BASE_REMOTA}/{contrato}"
     DOWNLOADS_DIR = Path.home() / "Downloads"
     pasta_local = Path(tempfile.gettempdir()) / f"temp_{contrato}"
     zip_nome = DOWNLOADS_DIR / f"{contrato}.zip"
 
-    sftp, transport = conectar_sftp()
+    sftp = None
+    transport = None
 
     try:
-        #verifica c a PASTA existe no servidor
-        try:
-            sftp.stat(pasta_remota)
-        except FileNotFoundError:
-            return f"O contrato {contrato} não existe (Pasta não encontrada no servidor)."
+        sftp, transport = conectar_sftp()
 
-        # a pasta existe. tentando baixar
+        pasta_remota = encontrar_pasta_contrato(sftp, BASE_REMOTA, contrato)
+        if not pasta_remota:
+            return f"Nenhuma pasta encontrada contendo: {contrato}"
+
         qtd_arquivos = baixar_recursivo(sftp, pasta_remota, pasta_local)
 
-        #see a pasta existe mas a contagem é 0
         if qtd_arquivos == 0:
             return f"A pasta do contrato {contrato} existe, mas está vazia (sem arquivos)."
 
-        # deu bom, vm zipar
         if zipar_pasta(pasta_local, zip_nome):
             return f"Contrato {contrato} baixado com sucesso!\nTotal de arquivos: {qtd_arquivos}\nSalvo em: {zip_nome}"
-        else:
-            return "Erro ao gerar o arquivo ZIP."
+
+        return "Erro ao gerar o arquivo ZIP."
 
     except Exception as e:
         return f"Erro inesperado: {e}"
 
     finally:
-        sftp.close()
-        transport.close()
+        if sftp:
+            sftp.close()
+        if transport:
+            transport.close()
         if pasta_local.exists():
             shutil.rmtree(pasta_local)
-
 
 def main():
     contrato = input("Qual o contrato? ").strip()
     resultado = executar_contrato(contrato)
     print(resultado)
-
 
 if __name__ == "__main__":
     main()
