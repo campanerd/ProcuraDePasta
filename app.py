5e9d2130115e55e0c64ed966513ac1faa3a7ef78
Davi Campaner <campanerdavi@gmail.com>
2026-02-24 15:54:42 -0300

    separate UI into frames and add mode-based dynamic content

--- app.py
+++ app.py
@@ -1,7 +1,37 @@
 import tkinter as tk
 from tkinter import messagebox
 import threading
-from main import executar_contrato
+from main import executar_contrato, executar_contrato_fast
+
+modo_execucao = None 
+entry = None
+botao = None
+
+
+nomes_modo = {
+    "normal": "Digitos finais do contrato",
+    "fast": "Nome exato da pasta do contrato"
+}
+
+def selecionar_opcao(modo):
+    global modo_execucao, entry, botao
+    modo_execucao = modo
+
+    limpar_frame_inferior()
+
+    tk.Label(frame_inferior, text="Contrato").pack(pady=5)
+
+    entry = tk.Entry(frame_inferior, width=30)
+    entry.pack()
+
+    botao = tk.Button(frame_inferior, text="Baixar", command=iniciar)
+    botao.pack(pady=10)
+
+    status.config(text=f"Modo selecionado: {nomes_modo[modo]}")
+
+def limpar_frame_inferior():
+    for widget in frame_inferior.winfo_children():
+        widget.destroy()
 
 def iniciar():
     contrato = entry.get().strip()
@@ -10,36 +40,70 @@ def iniciar():
         messagebox.showwarning("Aviso", "Informe o contrato")
         return
 
+    if not modo_execucao:
+        messagebox.showwarning("Aviso", "Selecione uma opção")
+        return
+
     status.config(text="Processando...")
     botao.config(state="disabled")
 
-    threading.Thread(
-        target=executar_em_thread,
-        args=(contrato,),
-        daemon=True
-    ).start()
+    if modo_execucao == "normal":
+        thread = threading.Thread(
+            target=executar_em_thread,
+            args=(contrato,),
+            daemon=True
+        )
+    else:
+        thread = threading.Thread(
+            target=executar_em_thread_fast,
+            args=(contrato,),
+            daemon=True
+        )
+
+    thread.start()
+
 
 def executar_em_thread(contrato):
     resultado = executar_contrato(contrato)
-
-    # atualização SEGURA da interface
     root.after(0, lambda: finalizar(resultado))
+
+
+def executar_em_thread_fast(contrato):
+    resultado = executar_contrato_fast(contrato)
+    root.after(0, lambda: finalizar(resultado))
+
 
 def finalizar(mensagem):
     status.config(text=mensagem)
     botao.config(state="normal")
 
+
+#janela
 root = tk.Tk()
 root.title("Download SFTP")
-root.geometry("450x220")
+root.geometry("450x280")
 
-tk.Label(root, text="Contrato").pack(pady=5)
+#frame superior
 
-entry = tk.Entry(root, width=30)
-entry.pack()
+frame_button = tk.Frame(root)
+frame_button.pack(pady=10)
 
-botao = tk.Button(root, text="Baixar", command=iniciar)
-botao.pack(pady=10)
+botao1 = tk.Button(frame_button, text="ESPECÍFICO", width=15, command=lambda: selecionar_opcao("normal"))
+botao1.pack(side="left", padx=5)
+
+botao2 = tk.Button(frame_button, text="RÁPIDO", width=15, command=lambda: selecionar_opcao("fast"))
+botao2.pack(side="left", padx=5)
+
+#frame inferior
+
+frame_inferior = tk.Frame(root)
+frame_inferior.pack(expand=True)
+
+tk.Label(
+    frame_inferior,
+    text="Selecione uma opção acima",
+    fg="gray"
+).pack(pady=20)
 
 status = tk.Label(
     root,
@@ -47,8 +111,11 @@ status = tk.Label(
     wraplength=420,
     justify="left"
 )
-status.pack(pady=10)
+status.pack(pady=5)
 
-tk.Label(root, text="Desenvolvido por Davi Campaner").pack(side="bottom", pady=5)
+tk.Label(
+    root,
+    text="Desenvolvido por Davi Campaner"
+).pack(side="bottom", pady=5)
 
 root.mainloop()
--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from down_service import baixar_recursivo
 
 BASE_REMOTA = "/sftp-assessoria-prd/Fortes-Assessoria/Cobaas/Enviados/Documentos"
 
-
 def encontrar_pasta_por_prefixo(sftp, base_remota, contrato):
     for attr in sftp.listdir_iter(base_remota):
         if attr.filename.startswith(contrato):
@@ -61,6 +60,45 @@ def executar_contrato(contrato: str) -> str:
         if pasta_local.exists():
             shutil.rmtree(pasta_local)
 
+def executar_contrato_fast(contrato: str) -> str:
+    if not contrato:
+        return "Contrato não informado."
+
+    pasta_remota = f"{BASE_REMOTA}/{contrato}"
+    DOWNLOADS_DIR = Path.home() / "Downloads"
+    pasta_local = Path(tempfile.gettempdir()) / f"temp_{contrato}"
+    zip_nome = DOWNLOADS_DIR / f"{contrato}.zip"
+
+    sftp, transport = conectar_sftp()
+
+    try:
+        #verifica c a PASTA existe no servidor
+        try:
+            sftp.stat(pasta_remota)
+        except FileNotFoundError:
+            return f"O contrato {contrato} não existe (Pasta não encontrada no servidor)."
+
+        # a pasta existe. tentando baixar
+        qtd_arquivos = baixar_recursivo(sftp, pasta_remota, pasta_local)
+
+        #see a pasta existe mas a contagem é 0
+        if qtd_arquivos == 0:
+            return f"A pasta do contrato {contrato} existe, mas está vazia (sem arquivos)."
+
+        # deu bom, vm zipar
+        if zipar_pasta(pasta_local, zip_nome):
+            return f"Contrato {contrato} baixado com sucesso!\nTotal de arquivos: {qtd_arquivos}\nSalvo em: {zip_nome}"
+        else:
+            return "Erro ao gerar o arquivo ZIP."
+
+    except Exception as e:
+        return f"Erro inesperado: {e}"
+
+    finally:
+        sftp.close()
+        transport.close()
+        if pasta_local.exists():
+            shutil.rmtree(pasta_local)
 
 def main():
     contrato = input("Qual o contrato? ").strip()
